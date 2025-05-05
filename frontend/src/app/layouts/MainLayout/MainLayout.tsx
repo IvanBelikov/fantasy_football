ad84a80231346e85569000bbb230b75b2f2322e2
Ivan Belikov <belikov.workmail@gmail.com>
2025-05-05 17:36:28 +0300

    refactor: MainPage path changed

--- frontend/src/app/layouts/MainLayout/MainLayout.tsx
+++ frontend/src/app/layouts/MainLayout/MainLayout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router'
 import { MainHeader } from '@base/widgets'
 
 import { LayoutContainer } from './MainLayout.styles'
-import { MainPage } from '@base/pages/MainPage/ui/MainPage'
+import { MainPage } from '@base/pages'
 
 export const MainLayout = () => (
   <LayoutContainer>
